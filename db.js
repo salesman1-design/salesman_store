3f5be7934c08ca8c1fb6f71a3ae491ed9e6ebcf5
Mr Salesman <GGSALESMAN001@gmail.com>
2025-06-10 17:56:39 -0500

    Initial rebuild 4

--- db.js
+++ db.js
@@ -5,11 +5,12 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: parseInt(process.env.MYSQL_PORT, 10),
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
 
 module.exports = pool;
