e055970cd7f3336e975dcfbca37d1f5212c23b14
Mr Salesman <GGSALESMAN001@gmail.com>
2025-06-10 11:26:53 -0500

    new changes to server and admin 2

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.static('public'));
